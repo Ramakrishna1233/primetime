83735487288f5bc123d99564ba89d1f8b2d4e78a
Ramakrishna1233 <88773627+Ramakrishna1233@users.noreply.github.com>
2021-08-11 17:25:03 +0530

    primetime

--- src/AppBundle/Resources/views/Subtitle/show.html.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$media = $subtitle->getMedia();
-$file_name = $media->getLink();
-$result = file_get_contents($media->getLink());
-header('Content-Type:text/vtt; charset=utf-8');
-echo $result;
-?>
